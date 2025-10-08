b922fd5af12769542c9dacd69f5ea60faa0425a1
MeilisaApriyani <melisa.2005.apriyani@gmail.com>
2025-10-08 20:41:55 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ import * as authController from "../controller/authController.js"
 
 const api = express.Router()
 
-api.post('/register', authController.register )
+api.post('/register', authController.register)
+api.post('/login', authController.login)
 
 export default api
