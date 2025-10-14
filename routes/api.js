4156dac03a4c81325502c97acc7eb49bcfd786d1
MeilisaApriyani <melisa.2005.apriyani@gmail.com>
2025-10-14 22:27:23 +0700

    Delete api.js

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import * as authController from "../controller/authController.js"
-
-const api = express.Router()
-
-api.post('/register', authController.register )
-
-export default api
