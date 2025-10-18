8167a4853bb804746650f69dd8103f102ee16ce0
Mei <melisa.2005.apriyani@gmail.com>
2025-10-18 18:21:56 +0700

    Update passport.js

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 import passport from "passport";
 
-import {ExtractJwt, Strategy} from "password-jwt"
+import {ExtractJwt, Strategy} from "password-jwt";
 
 const opts = {
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
